b14702e6d836ad845fe164abd3601f605887083f
Anik Gupta <anik.gupta@gmail.com>
2019-11-10 00:30:54 -0800

    changed Video class

--- Phyzmo/Phyzmo/Model/Video.swift
+++ Phyzmo/Phyzmo/Model/Video.swift
@@ -9,25 +9,33 @@
 import Foundation
 import UIKit
 import AVFoundation
+import Firebase
 class Video{
-    var id : Int
+    var id : String
     var thumbnail : UIImage
-    var videoURL : String
     var video : AVPlayer? // stays null until collection view is clicked
     var data : [String: [Double]]? // stays null until collection view is clicked
     var objects : [String]
     
     
-    init(id: Int, thumbnail: UIImage, videoURL: String, objects: [String]) {
+    init(id: String, thumbnail: UIImage, objects: [String]) {
         self.id = id
         self.thumbnail = thumbnail
-        self.videoURL = videoURL // MUST FIX FOR FIREBASE
         self.objects = objects
     }
     
     func contruct(){
-        self.video = AVPlayer(url: URL(fileURLWithPath: self.videoURL)) // MUST FIX FOR FIREBASE
-        APIClient.getObjectData(objectsDataUri: "dfaf", obj_descriptions: objects) { (data) in
+        // Create a reference to the file you want to download
+        let videoRef = Storage.storage().reference().child("NEED TO GET FROM PATRICK")
+        videoRef.downloadURL { url, error in
+          if let error = error {
+            // Handle any errors
+          } else {
+            self.video = AVPlayer(url: url!)
+          }
+        }
+
+        APIClient.getObjectData(objectsDataUri: "NEEDTOFIND-ASK IMRAN FROM CLOUD FUNCTION", obj_descriptions: objects) { (data) in
             self.data = data as! [String : [Double]]
         }
     }
